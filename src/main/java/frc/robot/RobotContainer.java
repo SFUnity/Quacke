c6033bcda0f4e8a08dd928e1c87a82dc6a204913
SF Unity <sfunityrobotics@gmail.com>
2026-01-31 13:36:32 -0800

    add commands to autochooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -96,6 +96,8 @@ public class RobotContainer {
         "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
     autoChooser.addOption(
         "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+    autoChooser.addOption("TuneModuleTurn", DriveCommands.tuneModuleTurn(drive));
+    autoChooser.addOption("TuneModuleDrive", DriveCommands.tuneModuleDrive(drive));
 
     // Configure the button bindings
     configureButtonBindings();
